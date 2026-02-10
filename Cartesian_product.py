340d7b2198224ba4a70eb8f66097a8f1e3beb900
Achyuth Jogi <achyuthnjogi621@gmail.com>
2026-02-10 10:32:43 +0530

    Day 3: Solved Cartesian Product Problem
    Added platform and problem description comments.

---
+++ Cartesian_product.py
@@ -0,0 +1,10 @@
+#Platform: HackerRank
+#Problem: Finding Cartesian Product 
+from itertools import product
+
+A = list(map(int, input().split()))
+B = list(map(int, input().split()))
+
+result = product(A, B)
+
+print(*result)
